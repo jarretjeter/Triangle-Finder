79a98d928c66b650a0fc67dd80325b8e5c4963d4
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-24 22:13:33 -0700

    create 'matchSides' function to compare input values of triangle sides

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
+function matchSides() {
+  const valueOne = parseInt($("#side1").val());
+  const valueTwo = parseInt($("#side2").val());
+  const valueThree = parseInt($("#side3").val());
+
+  if (valueOne === valueTwo && valueOne === valueThree) {
+    $("#equilateral").show();
+    $("#isosceles").hide();
+    $("#scalene").hide();
+  }
+    else if (valueOne === valueTwo && valueOne !== valueThree || valueOne === valueThree && valueOne !== valueTwo) {
+      $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+    }
+    else if (valueTwo === valueThree) {
+      $("#isosceles").show();
+      $("#scalene").hide();
+      $("#equilateral").hide();
+    }
+    else {
+      $("#scalene").show();
+      $("#isosceles").hide();      
+      $("#equilateral").hide();
+    }
+}
+
+
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
   event.preventDefault();
-  const valueOne = parseInt($("#value1").val());
-  const valueTwo = parseInt($("#value2").val());
-  const valueThree = parseInt($("#value3").val());
-});
+
+  matchSides();
+  });
 });
 
 
